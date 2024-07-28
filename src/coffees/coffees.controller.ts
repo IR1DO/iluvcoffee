978a7e99e2bf4d29b5c6a500452355efa5eba60f
Kevin Hu <hellokevinhu@icloud.com>
2024-07-28 20:58:16 +0800

    Route parameters, request body payload and response status codes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get('flavors')
+  /* DEPRECATED Underlying library response, hard to test, not recommended */
+  // findAll(@Res() response) {
+  //   return response.status(200).send('This action returns all coffees');
+  // }
+  findAll() {
+    return 'This action returns all coffees.';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee.`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee: ${body?.name}.`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee.`;
+  }
+}
